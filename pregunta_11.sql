78e9087f0b8a1f3a418982c0b0496d425292e052
Avanegasf93 <112149548+Avanegasf93@users.noreply.github.com>
2022-10-14 02:31:34 -0500

    Commit5
    Commit5

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -39,4 +39,4 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
 
-SELECT STRFTIME('%Y',c14) AS YEAR, COUNT(*) FROM tbl1 WHERE YEAR="2018"
+SELECT COUNT(*) FROM tbl1 WHERE strftime('%Y',c14)="2018"
